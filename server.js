dab115035fc107ea10f0966690ee95bd73185000
PROJECT HOPE <s7h2sft8bs@privaterelay.appleid.com>
2026-01-30 19:50:17 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/api/schedule-reminders', async (req, res) => {
+    res.json({ success: true, message: 'Reminders scheduled' });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
